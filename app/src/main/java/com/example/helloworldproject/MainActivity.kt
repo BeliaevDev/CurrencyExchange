5168280209636acd0d08c4f6a885e1dd7ec864e8
BeliaevDev <dev.beliaev@gmail.com>
2023-08-08 21:00:06 +0400

    Init new project

--- app/src/main/java/com/example/helloworldproject/MainActivity.kt
+++ app/src/main/java/com/example/helloworldproject/MainActivity.kt
@@ -1,25 +1,27 @@
 package com.example.helloworldproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
-import androidx.core.widget.doOnTextChanged
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.net.URL
-import java.net.URLConnection
-import kotlin.reflect.KType
-import kotlin.reflect.typeOf
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val numberInput = findViewById<EditText>(R.id.main_activity_value_from)
+        val valueFromText = findViewById<EditText>(R.id.main_activity_value_from)
+        val convertBtn = findViewById<Button>(R.id.main_activity_convert_btn)
         val exchangeRateValue = findViewById<TextView>(R.id.main_activity_rate_exchange)
         val currencyFromSpinner = findViewById<Spinner>(R.id.main_activity_currency_from)
         val currencyToSpinner = findViewById<Spinner>(R.id.main_activity_currency_to)
@@ -27,21 +29,13 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencies)
         currencyFromSpinner.adapter = adapter
         currencyToSpinner.adapter = adapter
-        currencyFromSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                exchangeRateValue.text = "${currencies[position]}/${currencyToSpinner.selectedItem.toString()}"
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // write code to perform some action
-            }
+        convertBtn.setOnClickListener {
+            val url = "https://currate.ru/api/?get=rates&pairs=${currencyFromSpinner.selectedItem}${currencyToSpinner.selectedItem}&key=cf4794f9809d6220e0bed03cf19705ea"
+            val json = try { URL(url).readText() } catch (e: Exception) { Log.e("Network", "error") }
+//            exchangeRateValue.text = json
+            Log.i("JSON", json.toString())
         }
-//        val selectedCurrencyFrom = currencyFromSpinner.selectedItem.toString()
-//        exchangeRateValue.text = selectedCurrencyFrom
-//        currencyFromSpinner
-////        val listItems = resources.getStringArray((R.array.currencies))
-////        spinner.
-//        val data = URL("http://data.fixer.io/api/latest?access_key =57583a2451bf0932d6c4c2f48bfcd893&base=USD&symbols=GBP,JPY,EUR")
 
+//        val data = URL("http://data.fixer.io/api/latest?access_key =57583a2451bf0932d6c4c2f48bfcd893&base=USD&symbols=GBP,JPY,EUR")
     }
 }
